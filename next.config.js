5fd1bd110f5918c69ae6b408db6204100a647833
Bartholomaeus Gillner <bgillner@llnw.com>
2022-06-03 18:17:06 +0200

    Remove code referencing an unsatisfied dependency

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
-// This file was automatically added by layer0 init.
-// You should commit this file to source control.
-const { withLayer0, withServiceWorker } = require('@layer0/next/config')
-
-const _preLayer0Export = {
+module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['static.tvmaze.com'],
   },
 }
-
-module.exports = (phase, config) =>
-  withLayer0(
-    withServiceWorker({
-      // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
-      // the logs in the Layer0 developer console.
-      layer0SourceMaps: true,
-
-      ..._preLayer0Export,
-    })
-  )
